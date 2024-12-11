2bcdebc259372629f136f731ec3f104c6158e800
Anton Raafat <138820743+tonyRaafat@users.noreply.github.com>
2024-12-11 23:57:07 +0200

    feat: implemented the GenericRepository

---
+++ Bulkify.Infrastructure/Repositories/GenericRepositories.cs
@@ -0,0 +1,51 @@
+﻿using Bulkify.Core.Entities;
+using Bulkify.Core.Interfaces.Repositories;
+using Bulkify.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulkify.Repository.Repositories
+{
+    public class GenericRepositories<T>:IGenericRepository<T> where T:BaseEntity
+    {
+        private readonly BulkifyDbContext _context;
+        public GenericRepositories(BulkifyDbContext context) 
+        { 
+            _context = context;
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
