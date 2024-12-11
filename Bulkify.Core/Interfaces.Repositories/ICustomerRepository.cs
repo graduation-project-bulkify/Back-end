abfac500bb915359a74ee9ae0af8aca04c2e1d89
Anton Raafat <138820743+tonyRaafat@users.noreply.github.com>
2024-12-12 00:54:57 +0200

    feat: create CustomerRepository

---
+++ Bulkify.Core/Interfaces.Repositories/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Bulkify.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulkify.Core.Interfaces.Repositories
+{
+    public interface ICustomerRepository:IGenericRepository<Customer>
+    {
+        Task<Customer?> GetCustomerByEmailAsync(string email);
+    }
+}
---
+++ Bulkify.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,24 @@
+﻿using Bulkify.Core.Entities;
+using Bulkify.Core.Interfaces.Repositories;
+using Bulkify.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulkify.Repository.Repositories
+{
+    public class CustomerRepository : GenericRepositories<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(BulkifyDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Customer?> GetCustomerByEmailAsync(string email)
+        {
+            return await _context.Customers.FirstOrDefaultAsync(c => c.Email == email);
+        }
+    }
+}
